67024badcd7006000f1e3000b573e5e215e35fa7
Roland Philippsen <roland dot philippsen at gmx dot net>
2010-01-11 12:13:35 -0800

    resurrect doxygen

--- rundox.sh
+++ rundox.sh
@@ -3,10 +3,14 @@
 echo "removing html directory and Doxyfile"
 rm -rf html Doxyfile
 
-STANFORD_WBC_REV=`svn info | awk '/^Revision:/{print $2}'`
+STANFORD_WBC_REV=`date`
+if [ -d .svn ]; then
+ STANFORD_WBC_REV="revision `svn info | awk '/^Revision:/{print $2}'`"
+fi
+STANFORD_WBC_REV=`echo ${STANFORD_WBC_REV} | sed 's: :_:g'`
 
-echo "substituting revisions stanford-wbc r${STANFORD_WBC_REV}"
-cat Doxyfile.in | sed -e s:@STANFORD_WBC_REV@:${STANFORD_WBC_REV}: > Doxyfile
+echo "substituting STANFORD_WBC_REV: ${STANFORD_WBC_REV}"
+cat Doxyfile.in | sed -e s/@STANFORD_WBC_REV@/${STANFORD_WBC_REV}/ > Doxyfile
 
 echo "running doxygen like this: doxygen Doxyfile 2>&1 | tee doxy.log"
 doxygen Doxyfile 2>&1 | tee doxy.log
