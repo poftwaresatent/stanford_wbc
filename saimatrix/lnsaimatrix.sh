c869108e20854b23b3c8e8ac56b644deb91c3e4e
Roland Philippsen <roland dot philippsen at gmx dot net>
2010-03-23 10:49:00 -0700

    Big file shuffling: each sub-project follow a general layout, with a subdirectory named after the project for the actual (library) sources, description files in the top-level, and some of the following subdirectories (depending on whether they are applicable):  * tests/ for (unit) tests  * applications/ for end-user executable programs  * doc/ for documentation  * maybe others, e.g. wbc/plugins

--- saimatrix/lnsaimatrix.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-test -z "$1" && echo "$0: require source directory as argument"
-test -z "$1" && exit 42
-
-test -L saimatrix && exit 0
-
-echo "Creating symbolic link to source directory $1"
-ln -s $1 saimatrix
