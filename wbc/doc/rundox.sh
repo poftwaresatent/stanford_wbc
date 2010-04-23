d9860f0c84a286ded1382d53b798dbe3c0e1270d
Roland Philippsen <roland dot philippsen at gmx dot net>
2010-04-22 18:43:11 -0700

    fix nightly doxy run

--- wbc/doc/rundox.sh
+++ wbc/doc/rundox.sh
@@ -3,14 +3,10 @@
 echo "removing html directory and Doxyfile"
 rm -rf html Doxyfile
 
-STANFORD_WBC_REV=`date`
-if [ -d .svn ]; then
- STANFORD_WBC_REV="revision `svn info | awk '/^Revision:/{print $2}'`"
-fi
-STANFORD_WBC_REV=`echo ${STANFORD_WBC_REV} | sed 's: :_:g'`
+STANFORD_WBC_REV=`git show --format="%H %aD" HEAD | head -n 1`
 
-echo "substituting STANFORD_WBC_REV: ${STANFORD_WBC_REV}"
-cat Doxyfile.in | sed -e s/@STANFORD_WBC_REV@/${STANFORD_WBC_REV}/ > Doxyfile
+echo "substituting revision ${STANFORD_WBC_REV}"
+cat Doxyfile.in | sed -e "s/@STANFORD_WBC_REV@/${STANFORD_WBC_REV}/" > Doxyfile
 
 echo "running doxygen like this: doxygen Doxyfile 2>&1 | tee doxy.log"
 doxygen Doxyfile 2>&1 | tee doxy.log
