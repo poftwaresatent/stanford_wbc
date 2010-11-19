00484b2eca04ab1bc5045d7c7ea890549f93b36f
Roland Philippsen <poftwaresatent@gmail.com>
2010-11-18 19:14:27 -0800

    quick hack sine wave generator

---
+++ jspace/applications/sinewave.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import numpy
+
+omega = numpy.array([1.0, 1.5])
+phi0 = numpy.array([0.3, -0.7])
+amp = numpy.array([1.5, 0.9])
+
+for tt in xrange(2000):
+    foo = 1e-3 * tt * omega + phi0
+    for pp in amp * numpy.sin(foo):
+        print pp,
+    for vv in amp * numpy.cos(foo):
+        print vv,
+    print
